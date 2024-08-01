2d774f7d462368cf40515ffdd296e6fffc73773a
SunSung W541 <mail@embedded.biz.ua>
2024-08-01 19:17:36 +0200

    master [v0.0.3]  added autotests for all popular platforms

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package main
 const (
 	GlobalName       string = "EspToolMod"
 	GlobalDateUpdate string = "08-01-2024"
-	GlobalHash       string = "48f1c1b22e7acf401fbf220341c5a73c5c50aecf"
+	GlobalHash       string = "7266007a00fdcd8c2bfaa224ecc3a57bb5ddd7b0"
 
-	GlobalVersion      string = "v0.0.2"
+	GlobalVersion      string = "v0.0.3"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 2
+	GlobalVersionPatch uint16 = 3
 )
