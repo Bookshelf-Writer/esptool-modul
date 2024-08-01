68f3b0d35fe8a098f94c3df02b74d31c500f8dba
SunSung W541 <mail@embedded.biz.ua>
2024-08-01 20:50:46 +0200

    master [v0.0.8]  debug

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package main
 const (
 	GlobalName       string = "EspToolMod"
 	GlobalDateUpdate string = "08-01-2024"
-	GlobalHash       string = "580c37584537ad88a5409cc932c1df4359d41250"
+	GlobalHash       string = "45aefb0e8f93d7aceece7ed21995d936ed4502fc"
 
-	GlobalVersion      string = "v0.0.7"
+	GlobalVersion      string = "v0.0.8"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 7
+	GlobalVersionPatch uint16 = 8
 )
