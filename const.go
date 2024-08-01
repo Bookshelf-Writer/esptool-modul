5a02fa9af6a057d918a2a5442d234d2c2b0baf2f
SunSung W541 <mail@embedded.biz.ua>
2024-08-01 21:00:14 +0200

    master [v0.0.11]  сборка под разные платформы

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package main
 const (
 	GlobalName       string = "EspToolMod"
 	GlobalDateUpdate string = "08-01-2024"
-	GlobalHash       string = "4bfe2049f862d41783d92cdcb3eab4d07886c1ca"
+	GlobalHash       string = "1cf4b942e9b6da3351120a26a1b79c3ee54dc48d"
 
-	GlobalVersion      string = "v0.0.10"
+	GlobalVersion      string = "v0.0.11"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 10
+	GlobalVersionPatch uint16 = 11
 )
