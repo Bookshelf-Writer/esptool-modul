a6162129442789fa48aae95f3af401ba94fd27ff
SunSung W541 <mail@embedded.biz.ua>
2024-08-01 22:23:47 +0200

    master [v0.11.0]  final autoasm

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package main
 const (
 	GlobalName       string = "EspToolMod"
 	GlobalDateUpdate string = "08-01-2024"
-	GlobalHash       string = "68bd07793f4fd585c48f0b2b3440f20dcfa97399"
+	GlobalHash       string = "05fb5aa964f227b72bd8cf3673fb77abfd906445"
 
-	GlobalVersion      string = "v0.9.1"
+	GlobalVersion      string = "v0.10.1"
 	GlobalVersionMajor string = "v0"
-	GlobalVersionMinor uint16 = 9
+	GlobalVersionMinor uint16 = 10
 	GlobalVersionPatch uint16 = 1
 )
