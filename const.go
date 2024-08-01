68bd07793f4fd585c48f0b2b3440f20dcfa97399
SunSung W541 <mail@embedded.biz.ua>
2024-08-01 22:12:28 +0200

    master [v0.9.0]  mac

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package main
 const (
 	GlobalName       string = "EspToolMod"
 	GlobalDateUpdate string = "08-01-2024"
-	GlobalHash       string = "2f686f5b2ffddfbae65c9e75508af2a891806be9"
+	GlobalHash       string = "8558f5ca0ce5aed300976126c88744cb283f81a8"
 
-	GlobalVersion      string = "v0.7.1"
+	GlobalVersion      string = "v0.7.2"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 7
-	GlobalVersionPatch uint16 = 1
+	GlobalVersionPatch uint16 = 2
 )
