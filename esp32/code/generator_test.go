86ba47348678a1e513e8739f7082c034b22aa796
SunSung W541 <mail@embedded.biz.ua>
2024-08-02 18:13:34 +0200

    master [v0.12.10]  transfer Feature

--- esp32/code/generator_test.go
+++ esp32/code/generator_test.go
@@ -7,13 +7,31 @@ import (
 
 //###########################################################//
 
-func TestErrorCode(t *testing.T) {
-	obj := generator.Init("Err", "error.go")
+func TestFeatureCode(t *testing.T) {
+	obj := generator.Init("Feature", "feature.go")
 	val := obj.GetVal()
 
 	//
 
-	val.Add(0000, "Received")
+	val.Add(1, "WiFi")
+	val.Add(2, "Bluetooth").Delim()
+
+	val.Add(31, "Single Core")
+	val.Add(32, "Dual Core").Delim()
+
+	val.Add(51, "80MHz")
+	val.Add(52, "160MHz")
+	val.Add(53, "240MHz")
+	val.Add(54, "320MHz").Delim()
+
+	val.Add(101, "Embedded Flash")
+	val.Add(102, "VRef calibration")
+	val.Add(103, "BLK3 partially reserved").Delim()
+
+	val.Add(151, "Coding Scheme None")
+	val.Add(152, "Coding Scheme 3/4")
+	val.Add(153, "Coding Scheme Repeat")
+	val.Add(154, "Coding Scheme Invalid")
 
 	//
 
