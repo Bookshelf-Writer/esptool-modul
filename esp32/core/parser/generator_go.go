efffc07188dc47e26a530615b54773d7e4b8e6b2
SunSung W541 <mail@embedded.biz.ua>
2024-08-02 04:56:05 +0200

    master [v0.12.7]  Generated

--- esp32/core/parser/generator_go.go
+++ esp32/core/parser/generator_go.go
@@ -4,6 +4,7 @@ import (
 	"esptool/common/generator"
 	"esptool/esp32/core"
 	"fmt"
+	"strconv"
 )
 
 //###########################################################//
@@ -11,9 +12,43 @@ import (
 func buildGO(maps map[string]*core.ModulStruct, namespace map[string]string) {
 	for filename, key := range namespace {
 		obj := generator.Init(key, "esp32/core/"+filename+".go")
+		buf, ok := maps[key]
+		if !ok {
+			continue
+		}
+		mod := *buf
 
-		obj.Print("var ").Print(key).PrintLN(" ModulStruct")
+		obj.Print("var ").Print(key).PrintLN(" = ModulStruct{")
+		obj.Repeat(1).Print("Name: ").PrintString(mod.Name).PrintLN(",").LN()
 
+		obj.Repeat(1).PrintLN("Sys: ModulSystemStruct{")
+		obj.Repeat(2).Print("UF2: " + strconv.FormatUint(mod.Sys.UF2, 10)).PrintLN(",")
+		obj.Repeat(2).Print("Chip: " + strconv.Itoa(mod.Sys.Chip)).PrintLN(",")
+		obj.Repeat(2).Print("LenStatus: " + strconv.Itoa(mod.Sys.LenStatus)).PrintLN(",")
+		obj.Repeat(2).Print("FlashOffset: " + strconv.FormatUint(mod.Sys.FlashOffset, 10)).PrintLN(",")
+		obj.Repeat(1).PrintLN("},")
+
+		obj.Repeat(1).PrintLN("Encrypt: ModulEncryptStruct{")
+		obj.Repeat(2).Print("WriteAlign: " + strconv.Itoa(mod.Encrypt.WriteAlign)).PrintLN(",")
+		obj.Repeat(2).Print("Supports: " + strconv.FormatBool(mod.Encrypt.Supports)).PrintLN(",")
+		obj.Repeat(1).PrintLN("},")
+
+		obj.Repeat(1).Print("MagicValue: []uint64{")
+		for _, number := range mod.MagicValue {
+			obj.Print(strconv.FormatUint(number, 10) + ",")
+		}
+		obj.PrintLN("},")
+
+		obj.Repeat(1).PrintLN("Memory: map[string]ModulMapStruct{")
+		for keyMemo, objMemo := range mod.Memory {
+			obj.Repeat(2).PrintString(keyMemo).Print(": {")
+			obj.Print(strconv.FormatUint(objMemo.Start, 10) + ",")
+			obj.Print(strconv.FormatUint(objMemo.End, 10))
+			obj.PrintLN("},")
+		}
+		obj.Repeat(1).PrintLN("},")
+
+		obj.PrintLN("}")
 		obj.SaveFileBuf("core")
 	}
 	fmt.Println("GO generated")
