0fc572b95e190abb8658f16be6a478a86dcb32f4
SunSung W541 <mail@embedded.biz.ua>
2024-08-02 22:07:39 +0200

    master [v0.13.4]  консоль перезаписана

---
+++ cli.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"time"
+)
+
+//###########################################################//
+
+type CliBaudObj struct {
+	Connect  *uint
+	Transfer *uint
+}
+
+type CliConnObj struct {
+	Timeout *time.Duration
+	Retries *uint
+}
+
+type CliFlashObj struct {
+	Compress *bool
+	Offset   *uint64
+	Size     *uint64
+	FilePath *string
+	Name     *string
+}
+
+////
+
+type CliObj struct {
+	Port *string
+
+	Baud  CliBaudObj
+	Conn  CliConnObj
+	Flash CliFlashObj
+}
+
+var CLI = CliObj{}
+
+////
+
+func init() {
+	CLI.Port = flag.String(CliValPort, "", CliValTextPort)
+	CLI.Baud.Connect = flag.Uint(CliValBaudConnect, 115200, CliValTextBaudConnect)
+	CLI.Baud.Transfer = flag.Uint(CliValBaudTransfer, 115200, CliValTextBaudTransfer)
+
+	CLI.Conn.Timeout = flag.Duration(CliValConnTimeout, 800*time.Millisecond, CliValTextConnTimeout)
+	CLI.Conn.Retries = flag.Uint(CliValConnRetries, 8, CliValTextConnRetries)
+
+	CLI.Flash.Compress = flag.Bool(CliValFlashCompress, false, CliValTextFlashCompress)
+	CLI.Flash.Offset = flag.Uint64(CliValFlashOffset, 0, CliValTextFlashOffset)
+	CLI.Flash.Size = flag.Uint64(CliValFlashSize, 0, CliValTextFlashSize)
+	CLI.Flash.FilePath = flag.String(CliValFlashFile, "", CliValTextFlashFile)
+	CLI.Flash.Name = flag.String(CliValFlashName, "PARTITION", CliValTextFlashName)
+}
