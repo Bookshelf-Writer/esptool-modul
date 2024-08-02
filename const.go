ad99ac4db805a7b75ddde89962e79527d60097ab
SunSung W541 <mail@embedded.biz.ua>
2024-08-02 21:37:02 +0200

    master [v0.13.3]  go:build ignore

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package main
 const (
 	GlobalName       string = "EspToolMod"
 	GlobalDateUpdate string = "08-02-2024"
-	GlobalHash       string = "b43a3e8c8f1cade9c49e2579ee78057670714aae"
+	GlobalHash       string = "f6b0deb2a482321b19da8ab8fd9c24aee4e5a1f1"
 
-	GlobalVersion      string = "v0.13.2"
+	GlobalVersion      string = "v0.13.3"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 13
-	GlobalVersionPatch uint16 = 2
+	GlobalVersionPatch uint16 = 3
 )
--- esp32/core/parser/const.go
+++ esp32/core/parser/const.go
@@ -1,3 +1,5 @@
+//go:build ignore
+
 package main
 
 const UrlPyDir = "https://raw.githubusercontent.com/espressif/esptool/master/esptool/targets/"
